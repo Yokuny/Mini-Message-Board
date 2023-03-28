2dfe86219e53716259eba120bfaa93edf3950431
yokuny <felipe.vni@hotmail.com>
2023-03-28 14:27:35 -0300

    new feature now the input, first request a user name, when the user put his name this is store on localStorage

--- routes/app.js
+++ routes/app.js
@@ -2,21 +2,30 @@ const express = require("express");
 const fs = require("fs/promises");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const { LocalStorage } = require("node-localstorage");
+const localStorage = new LocalStorage("./scratch");
 const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const user = "Felipe Rangel Ribeiro";
 const messages = [];
+let user = localStorage.getItem("user") || "";
+
 messages.push({ user: "Janete8749", message: "tuts tuts quero vê" });
 messages.push({ user: "Tripé de andaime", message: "Tudo em cima ?" });
 
 app.get("/", async (req, res) => {
-  console.count(">");
+  if (user.length === 0) {
+    username = "Digite seu nome:";
+    route = "username";
+  } else {
+    username = user;
+    route = "new";
+  }
   try {
     const indexPage = await fs.readFile("index.ejs", "utf-8");
     res.set("Content-Type", "text/html");
-    res.send(ejs.render(indexPage, { messages: messages }));
+    res.send(ejs.render(indexPage, { messages: messages, username, route }));
   } catch (err) {
     console.error(err);
   }
@@ -24,6 +33,11 @@ app.get("/", async (req, res) => {
 
 app.post("/new", (req, res) => {
   messages.push({ user: user, message: req.body.message });
+  res.redirect("/");
+});
+app.post("/username", (req, res) => {
+  user = req.body.message;
+  localStorage.setItem("user", user);
   res.redirect("/");
 });
 
